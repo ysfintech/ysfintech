82616344e7c1e590f35d4bca63e02529240ab5c4
Seunghwanly <seunghwanly@gmail.com>
2021-04-12 18:36:53 +0900

    EducationPage - firestore added.

--- yonsei_financial_tech/lib/pages/education/education.dart
+++ yonsei_financial_tech/lib/pages/education/education.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:yonsei_financial_tech/components/blog.dart';
 import 'package:yonsei_financial_tech/components/components.dart';
+// firestore
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class EduPage extends StatefulWidget {
   @override
@@ -9,6 +11,19 @@ class EduPage extends StatefulWidget {
 
 class _EduPageState extends State<EduPage> {
   ScrollController _controller = new ScrollController();
+
+  // fire store
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  CollectionReference edu =
+      FirebaseFirestore.instance.collection('education');
+  // data
+  var fetchedData;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchedData = edu.get();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,13 +41,36 @@ class _EduPageState extends State<EduPage> {
                 // IMAGE BACKGROUND - NAME -------------------------------------------
                 title(context),
                 // Education Article -------------------------------------------------
-                Article(
-                  false,
-                  backgroundColor: themeBlue,
-                  title: 'Education',
-                  content:
-                      '최신 금융연구에서 사용되는 다양한 연구 방법론과 적용 사례 등을 학습할 수 있는 금융기술전문가 교육과정을 진행합니다. 본 교육과정은 학계의 최신 이론 및 업계 전문가의 경험을 결합하여 실제 금융 관련 프로젝트나 사업에 적용 가능한 내용으로 구성됩니다.\n\n이를 위해, 국내외의 저명한 교수진과 업계 최고 수준의 전문가들로 구성된 강사진들이 주제별로 방법론 및 실전 사례 등을 단계적으로 구성하여 이해도를 높일 수 있도록 하였습니다. 또한, 교육과정에서 학습한 내용을 현업에서 더욱 발전시킬 수 있도록, 수강생과 강사진 간의 지속적인 네트워킹을 지원할 예정입니다.',
-                  
+                FutureBuilder<QuerySnapshot>(
+                  future: fetchedData,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Center(child: Text('500 - error'));
+                    } else if (!snapshot.hasData) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else {
+                      /**
+                       *  개행 문자를 포함해서 return 하기 
+                       */
+                      List<String> contentArray = snapshot.data.docs[0].data()['content'].toString().split('<br>');
+                      String content() {
+                        StringBuffer sb =new StringBuffer();
+                        for(String item in contentArray) {
+                          sb.write(item + '\n\n');
+                        }
+                        return sb.toString();
+                      }
+
+                      return Article(
+                        false,
+                        title: snapshot.data.docs[0].data()['title'],
+                        content: content(),
+                        backgroundColor: themeBlue
+                      );
+                    }
+                  },
                 ),
                 // Footer   ----------------------------------------------------------
                 Footer()
