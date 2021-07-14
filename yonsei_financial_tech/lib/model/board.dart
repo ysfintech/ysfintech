4e3f46285af837347c08be236d69f3d8b7858794
YonseiFintech <85093457+YonseiFintech@users.noreply.github.com>
2021-07-14 13:46:39 +0900

    Update board.dart

--- yonsei_financial_tech/lib/model/board.dart
+++ yonsei_financial_tech/lib/model/board.dart
@@ -41,3 +41,44 @@ class BoardItem {
         );
   }
 }
+
+class Works {
+  final List<WorkItem> list;
+
+  Works({this.list});
+
+  factory Works.fromJson(List<Map<String, dynamic>> json) {
+    List<WorkItem> boardList =
+        json.map((element) => WorkItem.fromJson(element)).toList();
+
+    return Works(list: boardList);
+  }
+}
+
+class WorkItem {
+  final int number;
+  final String title;
+  final String writer;
+  final String date;
+  final int view;
+  final String content;
+
+  WorkItem(
+      {this.content,
+      this.date,
+      this.number,
+      this.title,
+      this.view,
+      this.writer});
+
+  factory WorkItem.fromJson(Map<String, dynamic> json) {
+    return WorkItem(
+        number: json['number'],
+        title: json['title'],
+        writer: json['writer'],
+        date: json['date'],
+        view: json['view'],
+        content: json['content']
+        );
+  }
+}
