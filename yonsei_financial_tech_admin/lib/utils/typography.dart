6b1381e929f99d875fda5923bf1fa2dcd128497c
Seunghwanly <seunghwanly@gmail.com>
2022-01-22 14:00:44 +0900

    feat: ThemeTypography enum newly added
    get TextStyle from enum easily

--- yonsei_financial_tech_admin/lib/utils/typography.dart
+++ yonsei_financial_tech_admin/lib/utils/typography.dart
@@ -71,8 +71,8 @@ TextStyle buttonTextStyle = GoogleFonts.nunito(
 TextStyle imageDescTextStyle =
     GoogleFonts.nunito(textStyle: TextStyle(fontSize: 12, color: ligthGray));
 
-TextStyle imageDescTexWhitetStyle = GoogleFonts.nunito(
-    textStyle: TextStyle(fontSize: 12, color: lightWhite));
+TextStyle imageDescTexWhitetStyle =
+    GoogleFonts.nunito(textStyle: TextStyle(fontSize: 12, color: lightWhite));
 
 // Advanced
 TextStyle titleIntroductionTextStyle = TextStyle(
@@ -81,3 +81,36 @@ TextStyle titleIntroductionTextStyle = TextStyle(
     fontWeight: FontWeight.w800,
     // fontFamily: 'Yonsei'
     fontFamily: GoogleFonts.nunito().fontFamily);
+
+enum ThemeTyphography { title, subTitle, body, caption }
+
+extension ThemeTyphographyExtension on ThemeTyphography {
+  TextStyle get style {
+    switch (this) {
+      case ThemeTyphography.title:
+        return TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 38,
+        );
+      case ThemeTyphography.subTitle:
+        return TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: 20,
+        );
+      case ThemeTyphography.body:
+        return TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 14,
+        );
+      case ThemeTyphography.caption:
+        return TextStyle(
+          fontWeight: FontWeight.w300,
+          fontSize: 11,
+        );
+      default:
+        return TextStyle(
+          fontWeight: FontWeight.normal,
+        );
+    }
+  }
+}
