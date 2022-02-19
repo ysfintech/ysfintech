53d78244c9abb9b3bf55badca0f694ba6777c33c
Seunghwan Lee <seunghwanly@gmail.com>
2022-02-20 02:44:12 +0900

    refactor: remove admin

--- yonsei_financial_tech_admin/android/app/src/main/kotlin/com/example/the_basics/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.the_basics
-
-// removed at 02.19 cause V2 embedding issue occurred
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    // removed at 02.19 cause V2 embedding issue occurred
-}
