97a558df0dfe3f11460da1a8693685706319788a
YonseiFintech <85093457+YonseiFintech@users.noreply.github.com>
2024-01-21 02:32:34 +0900

    Update main.dart

--- yonsei_financial_tech/lib/main.dart
+++ yonsei_financial_tech/lib/main.dart
@@ -1,6 +1,3 @@
-library static_interop;
-import 'package:js/js.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
